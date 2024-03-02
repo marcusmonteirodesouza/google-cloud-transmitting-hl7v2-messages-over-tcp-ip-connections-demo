48594de1403224080763b6d4dd920498ee743a39
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2024-03-02 10:48:52 -0500

    Fix mllp_adapter_gce_container command

--- infra/deployment/terraform/deploy/modules/mllp_adapter/vm.tf
+++ infra/deployment/terraform/deploy/modules/mllp_adapter/vm.tf
@@ -9,8 +9,19 @@ module "mllp_adapter_gce_container" {
   version = "~> 3.1"
 
   container = {
-    command = "/usr/mllp_adapter/mllp_adapter --port=2575 --hl7_v2_project_id=${data.google_project.project.project_id} --hl7_v2_location_id=northamerica-northeast1 --hl7_v2_dataset_id=${var.hl7_v2_dataset_name} --hl7_v2_store_id=${var.hl7_v2_store_name} --api_addr_prefix=https://healthcare.googleapis.com:443/v1 --logtostderr --receiver_ip=0.0.0.0"
-    image   = "cloud-healthcare-containers/mllp-adapter:latest"
+    command = [
+      "/usr/mllp_adapter/mllp_adapter"
+    ]
+    args = [
+      "--port=2575",
+      "--hl7_v2_project_id=${data.google_project.project.project_id}",
+      "--hl7_v2_location_id=northamerica-northeast1",
+      "--hl7_v2_dataset_id=${var.hl7_v2_dataset_name}",
+      "--hl7_v2_store_id=${var.hl7_v2_store_name}",
+      "--logtostderr",
+      "--receiver_ip=0.0.0.0"
+    ]
+    image = "gcr.io/cloud-healthcare-containers/mllp-adapter:latest"
   }
 
   restart_policy = "Always"
