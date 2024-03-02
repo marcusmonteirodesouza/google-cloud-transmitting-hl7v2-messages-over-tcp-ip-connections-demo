bbab8b3730a174dd063231fff54f52d5625395c2
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2024-03-02 09:47:41 -0500

    Add allow-ingress-from-northamerica-northeast1-subnetwork firewall rule

--- infra/deployment/terraform/deploy/modules/network/firewall.tf
+++ infra/deployment/terraform/deploy/modules/network/firewall.tf
@@ -16,3 +16,22 @@ resource "google_compute_firewall" "vpc_allow_ingress_from_iap" {
     "35.235.240.0/20"
   ]
 }
+
+resource "google_compute_firewall" "vpc_allow_ingress_from_northamerica_northeast1_subnetwork" {
+  # https://cloud.google.com/iap/docs/using-tcp-forwarding#create-firewall-rule
+  name      = "allow-ingress-from-northamerica-northeast1-subnetwork"
+  network   = google_compute_network.vpc.name
+  direction = "INGRESS"
+
+  allow {
+    protocol = "all"
+  }
+
+  source_ranges = [
+    google_compute_subnetwork.vpc_northamerica_northeast1.ip_cidr_range
+  ]
+
+  target_tags = [
+    "allow-ingress-from-northamerica-northeast1-subnetwork"
+  ]
+}
